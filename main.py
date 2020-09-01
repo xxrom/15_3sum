eb57dfa114253128e60a185b32e81229f05192ac
Chernyshov_Nikita <xpom5566@mail.ru>
2020-09-01 22:00:39 +0300

    solved: but not by me =)

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
+# URL: explanation and solution
+# https://leetcode.com/problems/3sum/discuss/7498/Python-solution-with-detailed-explanationzzu
 from typing import List
 
 class Solution:
   def getKey(self, keys: List[int]):
-    return ''.join(str(sorted(keys)))
-
-  def findAns(self, path: List[int], others: List[int], curSum: int):
-    # print(path)
-    if len(path) == 3:
-      if curSum == 0:
-        keys = self.getKey(path)
-        if keys not in self.ans:
-          self.ans[keys] = path
-          # print('FOUND!')
-
-    if len(path) <= 2:
-      twoPath = self.getKey(path)
-
-      if len(path) == 2 and twoPath in self.memo:
-        return
-
-      self.memo[twoPath] = True
-
-      for index, num in enumerate(others):
-        if len(path) == 2 and num > -1 * curSum:
-          # print('EXIT')
-          break
-
-        self.findAns(path + [num], others[:index] + others[index + 1:], curSum + num)
+    return ''.join(str(keys))
 
   def threeSum(self, nums: List[int]) -> List[List[int]]:
-    sortedNums = sorted(nums)
+    nums.sort()
 
-    # print(sortedNums)
+    size = len(nums)
 
     self.ans = {}
-    self.memo = {}
-    for index, num in enumerate(sortedNums):
-      self.findAns([num], sortedNums[:index] + sortedNums[index + 1:], num)
-    # print(self.ans)
+    for i in range(size - 2): # -2 - because of left and right need place to be
+      # Imagine we are at index i and we have invoked the 2SUM problem from index i+1
+      # to end of the array. Now once the 2SUM terminates, we will have a list of all
+      # triplets which include nums[i]. To avoid duplicates, we must skip all nums[i]
+      # where nums[i] == nums[i-1].
+      if i > 0 and nums[i] == nums[i - 1]:
+        continue
+
+      l = i + 1
+      r = size - 1
+
+      while l < r:
+        if nums[l] + nums[r] < -nums[i]:
+          l = l + 1
+        elif nums[l] + nums[r] > -nums[i]:
+          r = r - 1
+        else:
+          # print([l, i, r])
+          keys = [nums[l], nums[i], nums[r]]
+          keys.sort()
+          self.ans[self.getKey(keys)] = keys
+
+          # Say index s and e are forming a solution in a sorted array. Now givens nums[s],
+          # there is a unique nums[e] such that nums[s]+nums[e]=Target. Therefore, if nums[s+1]
+          # is the same as nums[s], then searching in range s+1 to e will give us a duplicate
+          # solution. Thus we must move s till nums[s] != nums[s-1] to avoid getting duplicates.
+          while l < r and nums[l] == nums[l+1]:
+            l = l + 1
+          while l < r and nums[r] == nums[r - 1]:
+            r = r - 1
+
+          l = l + 1
+          r = r - 1
 
     return list(self.ans.values())
 
-my = Solution()
-n = [-1, 0, 1, 2, -1, -4]
 
+my = Solution()
+# n = [-1, 0, 1, 2, -1, -4]
+n = [0, 0, 0]
 trueAns=[
   [-1, 0, 1],
   [-1, -1, 2]
@@ -51,3 +59,12 @@ trueAns=[
 
 ans = my.threeSum(n)
 print("ans", ans, ans == trueAns)
+
+# a + b + c = 0
+# a + c = -b
+
+# a + c < -b => increment a + c sum => a index ++
+# a + c > -b => decrement a + c sum => c index --
+
+# Runtime: 1064 ms, faster than 60.28% of Python3 online submissions for 3Sum.
+# Memory Usage: 17.9 MB, less than 16.22% of Python3 online submissions for 3Sum.
